94b47e272d433688020870dafcb70b50efe78f90
Nyoman Sunima <nyomansunima@gmail.com>
2025-02-07 14:49:43 +0800

    🚜 refactor: remove the custom handle function, use link instead

--- src/features/contact/contact-item.tsx
+++ src/features/contact/contact-item.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import * as React from 'react'
-import { Button, toast } from '@shared/components'
+import { Button } from '@shared/components'
+import Link from 'next/link'
 
 export type ContactItemData = {
   href: string
@@ -14,34 +15,17 @@ type ContactItemProps = {
 
 export function ContactItem({ contact }: ContactItemProps) {
   const { href, label } = contact
-  const isEmail = href.includes('@gmail.com')
-
-  function copyEmailToClipboard(): void {
-    const email = href
-
-    navigator.clipboard
-      .writeText(email)
-      .then(() => {
-        toast('Nice, Email already copied to clipboard')
-      })
-      .catch(() => {
-        toast('Opps, Cannot copy the email')
-      })
-  }
-
-  function handleOnClick(): void {
-    if (isEmail) copyEmailToClipboard()
-    else window.open(href)
-  }
 
   return (
     <Button
       variant={'outline'}
       size={'base'}
       className="h-12 transition-all duration-300 hover:-translate-y-1 bg-surface text-[.8125rem]"
-      onClick={handleOnClick}
+      asChild
     >
-      {label}
+      <Link href={href} target="_blank">
+        {label}
+      </Link>
     </Button>
   )
 }
