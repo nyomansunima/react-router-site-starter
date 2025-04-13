43551f9ee4808a4f0dcc72df21fc7299edbafcf5
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-13 16:16:09 +0800

    chore: update deps

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ['@flaticon/flaticon-uicons'],
-  },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
-      { hostname: 'cdn.dribbble.com' },
       { hostname: 'cdn.hashnode.com' },
       { hostname: 'images.unsplash.com' },
-      { hostname: 'via.placeholder.com' },
     ],
   },
   skipTrailingSlashRedirect: true,
-  transpilePackages: [],
 }
 
 export default nextConfig
