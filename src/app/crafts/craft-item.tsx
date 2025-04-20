79b0642570b1bf4b96f87acca0ef5fae96d4433a
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-20 22:07:35 +0800

    refactor: wrap with border and div

--- src/app/crafts/craft-item.tsx
+++ src/app/crafts/craft-item.tsx
@@ -18,19 +18,21 @@ export function CraftItem({ craft }: CraftItemProps): React.ReactElement {
     <Link
       href={link}
       target="_blank"
-      className="flex flex-col h-full p-3 rounded-2xl border border-border border-dashed bg-surface relative group transition-all duration-300 hover:-translate-y-1"
+      className="flex p-1 rounded-2xl bg-surface relative group transition-all duration-300 hover:-translate-y-1"
     >
-      <div className="flex gap-2">
-        <i className="fi fi-br-circle-small text-foreground/60" />
-        <h3 className="text-sm font-medium">{title}</h3>
-      </div>
+      <div className="flex flex-col h-full w-full p-3 rounded-xl border-2 border-border border-dashed">
+        <div className="flex gap-2">
+          <i className="fi fi-br-circle-small text-foreground/60" />
+          <h3 className="text-sm font-medium">{title}</h3>
+        </div>
 
-      <p className="text-sm mt-3 text-foreground/60 line-clamp-2 text-pretty leading-relaxed">
-        {description}
-      </p>
+        <p className="text-sm mt-3 text-foreground/60 line-clamp-2 text-pretty leading-relaxed">
+          {description}
+        </p>
 
-      <div className="hidden w-7 h-7 rounded-lg bg-secondary/30 text-foreground/60c border border-border group-hover:flex justify-center items-center absolute top-3 right-3">
-        <i className="fi fi-br-arrow-small-right -rotate-45" />
+        <div className="hidden w-7 h-7 rounded-lg bg-secondary/30 text-foreground/60c border border-border group-hover:flex justify-center items-center absolute top-3 right-3">
+          <i className="fi fi-br-arrow-small-right -rotate-45" />
+        </div>
       </div>
     </Link>
   )
