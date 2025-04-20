11a344db557ae967149c71745dda6e664babdbcd
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-20 22:06:38 +0800

    refactor: wrap with border and div

--- src/app/resources/source-item.tsx
+++ src/app/resources/source-item.tsx
@@ -18,19 +18,21 @@ export function SourceItem({ source }: SourceItemProps): React.ReactElement {
     <Link
       href={url}
       target="_blank"
-      className="flex flex-col bg-surface border border-border border-dashed p-3 rounded-2xl col-span-1 transition-all duration-300 hover:-translate-y-1 group relative"
+      className="flex bg-surface p-1 rounded-2xl col-span-1 transition-all duration-300 hover:-translate-y-1 group relative"
     >
-      <div className="flex items-center gap-2">
-        <i className="fi fi-br-circle-small text-foreground/60" />
-        <h3 className="text-sm font-medium">{title}</h3>
-      </div>
+      <div className="flex flex-col w-full h-full border-2 border-border border-dashed rounded-xl p-3">
+        <div className="flex items-center gap-2">
+          <i className="fi fi-br-circle-small text-foreground/60" />
+          <h3 className="text-sm font-medium">{title}</h3>
+        </div>
 
-      <p className="text-sm text-foreground/60 mt-3 line-clamp-2 text-pretty leading-relaxed">
-        {description}
-      </p>
+        <p className="text-sm text-foreground/60 mt-3 line-clamp-2 text-pretty leading-relaxed">
+          {description}
+        </p>
 
-      <div className="hidden w-7 h-7 rounded-lg bg-secondary/30 text-foreground/60 border border-border group-hover:flex justify-center items-center absolute top-3 right-3">
-        <i className="fi fi-br-arrow-small-right -rotate-45" />
+        <div className="hidden w-7 h-7 rounded-lg bg-secondary/30 text-foreground/60 border border-border group-hover:flex justify-center items-center absolute top-3 right-3">
+          <i className="fi fi-br-arrow-small-right -rotate-45" />
+        </div>
       </div>
     </Link>
   )
