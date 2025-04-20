61e52a9b8198e4349783d17b7f96115215817723
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-20 22:03:16 +0800

    refactor: update text content

--- src/shared/components/common/footer.tsx
+++ src/shared/components/common/footer.tsx
@@ -4,8 +4,8 @@ export function Footer(): React.ReactElement {
   return (
     <footer className="flex flex-col" id="footer">
       <div className="flex flex-col tablet:flex-row tablet:items-center py-7 tablet:justify-between mt-8">
-        <span className="text-sm cursor-pointer">&copy; Nyoman Sunima.</span>
-        <span className="text-sm cursor-pointer">Design in Bali.</span>
+        <span className="text-sm cursor-pointer">&copy; Nyoman Sunima</span>
+        <span className="text-sm cursor-pointer">Design in Bali</span>
       </div>
     </footer>
   )
