8e367c6474e4bfe7056f2367405a5e0242f5ba2d
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-20 22:05:49 +0800

    refactor: wrap with border and div

--- src/app/stories/publication-item.tsx
+++ src/app/stories/publication-item.tsx
@@ -19,13 +19,15 @@ export function PublicationItem({
     <Link
       href={url}
       target="_blank"
-      className="flex items-center gap-2 bg-surface border border-border border-dashed p-3 rounded-2xl col-span-1 transition-all duration-300 hover:-translate-y-1 group relative"
+      className="flex bg-surface p-1 rounded-2xl col-span-1 transition-all duration-300 hover:-translate-y-1 group relative"
     >
-      <i className="fi fi-br-circle-small text-foreground/60" />
-      <h3 className="text-sm font-medium">{title}</h3>
+      <div className="flex items-center h-full w-full border-2 gap-2 p-3 rounded-xl border-border border-dashed">
+        <i className="fi fi-br-circle-small text-foreground/60" />
+        <h3 className="text-sm font-medium">{title}</h3>
 
-      <div className="hidden w-7 h-7 rounded-lg bg-secondary/30 text-foreground/60 border border-border group-hover:flex justify-center items-center absolute top-2 right-3">
-        <i className="fi fi-br-arrow-small-right -rotate-45" />
+        <div className="hidden w-7 h-7 rounded-lg bg-secondary/30 text-foreground/60 border border-border group-hover:flex justify-center items-center absolute top-2 right-3">
+          <i className="fi fi-br-arrow-small-right -rotate-45" />
+        </div>
       </div>
     </Link>
   )
