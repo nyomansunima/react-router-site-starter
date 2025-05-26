3a114bed0d8e4703f7f72aee0b0a5ab3cb3f35e5
Nyoman Sunima <nyomansunima@gmail.com>
2025-05-26 12:12:49 +0800

    refactor: add dialog integration

--- src/features/newsletter/newsletter-form.tsx
+++ src/features/newsletter/newsletter-form.tsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import {
   Button,
   Form,
@@ -11,6 +12,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { subscribeToNewsletter } from "./newsletter-service"
+import { SuccessNewsletterDialog } from "./success-dialog"
 
 const formSchema = z.object({
   email: z
@@ -20,6 +22,7 @@ const formSchema = z.object({
 })
 
 export function NewsletterForm() {
+  const [isOpenDialog, setIsOpenDialog] = React.useState(false)
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,6 +34,7 @@ export function NewsletterForm() {
     subscribeToNewsletter({ email: values.email })
       .then(() => {
         form.reset()
+        setIsOpenDialog(true)
       })
       .catch(() => {
         form.setError("email", { message: "Opps, something happen" })
@@ -38,27 +42,34 @@ export function NewsletterForm() {
   }
 
   return (
-    <Form {...form}>
-      <div className="w-full">
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col tablet:flex-row gap-3 w-full"
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <FormControl>
-                  <Input placeholder="Your email address" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Subscribe to newsletter</Button>
-        </form>
-      </div>
-    </Form>
+    <>
+      <Form {...form}>
+        <div className="w-full">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col tablet:flex-row gap-3 w-full"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input placeholder="Your email address" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Subscribe to newsletter</Button>
+          </form>
+        </div>
+      </Form>
+
+      <SuccessNewsletterDialog
+        open={isOpenDialog}
+        onOpenChange={setIsOpenDialog}
+      />
+    </>
   )
 }
