861973a00a596cc9bd9b0169a08d7b77e76927b3
Nyoman Sunima <nyomansunima@gmail.com>
2025-05-26 12:11:11 +0800

    refactor: update footer style

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -1,9 +1,7 @@
-import * as React from "react"
-
 export function Footer() {
   return (
     <footer className="flex flex-col" id="footer">
-      <div className="flex flex-col tablet:flex-row tablet:items-center py-7 tablet:justify-between mt-8">
+      <div className="flex flex-row items-center py-7 justify-between mt-8">
         <span className="text-sm cursor-pointer">&copy; Nyoman Sunima</span>
         <span className="text-sm cursor-pointer">Design in Bali</span>
       </div>
