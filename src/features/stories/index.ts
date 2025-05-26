09df08d207ae4c589b107689ab16eaf639e1bbfa
Nyoman Sunima <nyomansunima@gmail.com>
2025-05-26 12:12:08 +0800

    feat: add newsletter

--- src/features/stories/index.ts
+++ src/features/stories/index.ts
@@ -2,6 +2,7 @@ import storiesData from "./stories-data.json"
 
 export * from "./publication-section"
 export * from "./recent-section"
+export * from "./newsletter-section"
 export * from "./recent-item"
 
 export { storiesData }
