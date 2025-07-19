329672e6584a55b6540e666eb1bd992f363889ee
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-19 11:38:07 +0800

    fix: remove unused functions

--- src/shared/libs/utils.ts
+++ src/shared/libs/utils.ts
@@ -1,17 +1,7 @@
-import dayjs from "dayjs"
-import { type ClassValue, clsx } from "clsx"
+import { clsx } from "clsx"
+import type { ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import matter, { type GrayMatterFile } from "gray-matter"
 
 export function mergeClass(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function parseReadableDate(date: string) {
-  return dayjs(date).format("MMM, DD YYYY")
-}
-
-export function parseMarkdown(rawFile: any): GrayMatterFile<any> {
-  const parsedFile = matter(rawFile)
-  return parsedFile
 }
